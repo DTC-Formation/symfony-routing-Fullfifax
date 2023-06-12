3db7541cb08bfa950369f4a1675ac60de08b77de
Fullfifax <fullfifax1245@gmail.com>
2023-06-12 15:17:16 +0000

    [EXO] add twig path & result redirection

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -12,17 +12,6 @@ class HomepageController extends AbstractController
     #[Route('/', name: 'app_homepage')]
     public function index(Request $request): Response
     {
-
-        $nombreA = $request->get(key: 'nombre-a');
-        $nombreB = $request->get(key: 'nombre-b');
-        $signe = $request->get(key: 'signe');
-
-        $total = (is_numeric($nombreA) && is_numeric($nombreB)) 
-            ? eval("return $nombreA $signe $nombreB;") 
-            : "Impossible de faire le calcul";
-        
-        return $this->render('homepage/index.html.twig', [
-            'total' => $total,
-        ]);
+        return $this->render('homepage/index.html.twig');
     }
 }
---
+++ src/Controller/ResultController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ResultController extends AbstractController
+{
+    #[Route('/result', name: 'app_result')]
+    public function index(Request $request): Response
+    {
+        
+        $nombreA = $request->get(key: 'nombre-a');
+        $nombreB = $request->get(key: 'nombre-b');
+        $signe = $request->get(key: 'signe');
+        $operateurs = ['+', '-', '*', '/'];
+
+        $total = (is_numeric($nombreA) && is_numeric($nombreB) && in_array($signe, $operateurs))
+            ? (($total = eval("return $nombreA $signe $nombreB;")))
+            : "Impossible de faire le calcul";
+
+        return $this->render('result/index.html.twig', [
+            'total' => $total,
+        ]);
+    }
+}
